ffc5b2c54282c0ec537f132a7394f85fad75fc35
sky <skybride@hotmail.com>
2022-09-25 11:11:53 -0400

    Clean up Main Vue Layout

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <template>
+  <div>
+    <h1>My To Do</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+<!-- <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
@@ -26,4 +42,4 @@ export default class App extends Vue {}
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
